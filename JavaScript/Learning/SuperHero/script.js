f8c7671c128db8e2145b89dd85817f11854f9e17
f <franklinrohan2002@gmail.com>
2023-06-10 23:30:02 +0530

    chore:added a search button based on the input field

--- JavaScript/Learning/SuperHero/script.js
+++ JavaScript/Learning/SuperHero/script.js
@@ -3,6 +3,8 @@ const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`;
 
 const newHero = document.getElementById("herobtn");
 const heroImgDiv = document.getElementById("heroImg");
+const searchBTn = document.getElementById("searchHero");
+const searchInput = document.getElementById("searchInput");
 
 const getSuperHero = (id, name) => {
   fetch(`${BASE_URL}/${id}`)
@@ -16,9 +18,21 @@ const getSuperHero = (id, name) => {
     });
 };
 
+const getSearchedSuperHero = (name) => {
+  console.log(searchInput.value);
+  fetch(`${BASE_URL}/search/${name}`)
+    .then((response) => response.json())
+    .then((json) => {
+      const hero = json.results[0];
+      console.log(hero);
+      heroImgDiv.innerHTML = `<img src="${hero.image.url}" height=200 width=200>`;
+    });
+};
+
 const randomHero = () => {
   const nuberOfHero = 731;
   return Math.floor(Math.random() * nuberOfHero) + 1;
 };
 
 newHero.onclick = () => getSuperHero(randomHero());
+searchBTn.onclick = () => getSearchedSuperHero(searchInput.value);
